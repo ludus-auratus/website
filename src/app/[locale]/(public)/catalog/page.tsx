1d9754f08d22dab68c7dd7043c51b405c2eb8d32
Kayque Silva <67653975+kayqpereira@users.noreply.github.com>
2025-11-21 22:40:39 -0300

    feat: adiciona página do catálogo usando os novos componentes

--- src/app/[locale]/(public)/catalog/page.tsx
+++ src/app/[locale]/(public)/catalog/page.tsx
@@ -1,5 +1,27 @@
-import { redirect } from "next/navigation";
+"use client";
 
-export default function Catalog() {
-  redirect("/wip");
+import { useState } from "react";
+
+import games from "@/assets/data/games.json";
+import { Catalog } from "@/components/pages/catalog";
+
+export default function CatalogPage() {
+  const [sortBy, setSortBy] = useState("popular");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showFilters, setShowFilters] = useState(false);
+
+  return (
+    <Catalog.Root>
+      <Catalog.Header
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+        showFilters={showFilters}
+        setShowFilters={setShowFilters}
+      />
+
+      <Catalog.Grid games={games} showFilters={showFilters} />
+    </Catalog.Root>
+  );
 }
